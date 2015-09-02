ced2d2e06c4a6c34351569431ffee02d1a2865a7
aditmehta <aditmehta10@gmail.com>
2015-09-02 15:56:34 +0530

    shows number preview of random generated number

--- assets/javascripts/roulette.js
+++ assets/javascripts/roulette.js
@@ -1,12 +1,33 @@
 $(document).ready(function() {
   $('.spin').click(function() {
-    number_to_guess();
-  })
+    "The generated number is" + number_to_guess();
+  }); 
 })
 
+$(function() {
+  $('.zero, .range3, .range2, .which-row').click( function() {
+    $(this).css('background', 'blue')
+  });
+  $('.zero, .range3, .range2, .which-row').dblclick( function() {
+    $(this).css('background', 'transparent')
+  });
+  $('.red').click( function() {
+    $(this).css('background', 'blue')
+  });
+  $('.red').dblclick( function() {
+    $(this).css('background', 'red')
+  });
+  $('.black').click( function() {
+    $(this).css('background', 'blue')
+  });
+  $('.black').dblclick( function() {
+    $(this).css('background', 'black')
+  });
+});
 
 function number_to_guess() {
   var limit= 37;
   var number_random = Math.floor(Math.random() * limit);
   console.log(number_random);
+  $('.preview').html(number_random);
 }
