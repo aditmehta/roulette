c27ecda6916f83b4d9881f2d5ea73fb922c3fea9
shenils <shenil@bu.edu>
2015-09-03 13:53:36 +0530

    new layout js file

--- assets/javascripts/roulette.js
+++ assets/javascripts/roulette.js
@@ -1,56 +1,61 @@
-var selected_numbers = [];
-var unique_selection = [];
-var index = -1;
+var Game = {
+  init: function() {
+    $('.spin').click(function() {
+      Game.numberToGuess();
+    }); 
+    $('.num, .zero, .which-row, .range2, .range3').click(function() {
+      Game.selectNumber($(this).html());
+    });
+    $('.num, .zero, .which-row, .range2, .range3').dblclick(function() {
+      Game.removeSelection($(this).html());
+    });
+    $('.zero, .range3, .range2, .which-row').click( function() {
+      $(this).css('background', 'blue')
+    });
+    $('.zero, .range3, .range2, .which-row').dblclick( function() {
+      $(this).css('background', 'transparent')
+    });
+    $('.red').click( function() {
+      $(this).css('background', 'blue')
+    });
+    $('.red').dblclick( function() {
+      $(this).css('background', 'red')
+    });
+    $('.black').click( function() {
+      $(this).css('background', 'blue')
+    });
+    $('.black').dblclick( function() {
+      $(this).css('background', 'black')
+    });
+  },
+  userSelection: [],
+  red: ["1", "3", "5", "7", "9", "12", "14", "16", "18", "19", "21", "23", "25", "27", "30", "32", "34", "36"],
+  black: ["2", "4", "6", "8", "10", "11", "13", "15", "17", "20", "22", "24", "26", "28", "29", "31", "33", "35"],
+  thirdRow: ["3", "6", "9", "12", "15", "18", "21", "24", "27", "30", "33", "36"],
+  secondRow: ["2", "5", "8", "11", "14", "17", "20", "23", "26", "29", "32", "35"],
+  firstRow: ["1", "4", "7", "10", "13", "16", "19", "22", "25", "28", "31", "34"],
+
+  selectNumber: function(num) {
+    if(Game.userSelection.indexOf(num) == -1) {
+      Game.userSelection.push(num);
+    }
+    console.log(Game.userSelection);
+  },
+  removeSelection: function(num) {
+    var ind = Game.userSelection.indexOf(num);
+    if(ind != -1) {
+      Game.userSelection.splice(ind, 1);
+    }
+    console.log(Game.userSelection);
+  },
+  numberToGuess: function() {
+    var limit= 37;
+    var numberRandom = Math.floor(Math.random() * limit);
+    console.log(numberRandom);
+    $('.preview').html("THE RANDOM NUMBER THAT HAS BEEN GENERATED IS = " + numberRandom);  
+  }
+};
 
 $(document).ready(function() {
-  $('.spin').click(function() {
-    number_to_guess();
-  }); 
-  $('.num, .zero, .which-row, .range2, .range3').click(function() {
-    selected_numbers.push($(this).html());
-    unique_selection = selected_numbers.reduce(function(a,b) {
-      if (a.indexOf(b) < 0) a.push(b);
-      return a;
-    }, []);
-    console.log(unique_selection);
-  });
-  $('.num, .zero, .which-row, .range2, .range3').dblclick(function() {
-    index = unique_selection.indexOf($(this).html());
-    if (index > -1) {
-      splice_index(index, unique_selection);
-    };
-  });
-})
-
-$(function() {
-  $('.zero, .range3, .range2, .which-row').click( function() {
-    $(this).css('background', 'blue')
-  });
-  $('.zero, .range3, .range2, .which-row').dblclick( function() {
-    $(this).css('background', 'transparent')
-  });
-  $('.red').click( function() {
-    $(this).css('background', 'blue')
-  });
-  $('.red').dblclick( function() {
-    $(this).css('background', 'red')
-  });
-  $('.black').click( function() {
-    $(this).css('background', 'blue')
-  });
-  $('.black').dblclick( function() {
-    $(this).css('background', 'black')
-  });
+  Game.init();
 });
-
-function number_to_guess() {
-  var limit= 37;
-  var number_random = Math.floor(Math.random() * limit);
-  console.log(number_random);
-  $('.preview').html("THE RANDOM NUMBER THAT HAS BEEN GENERATED IS = " + number_random);
-}
-
-function splice_index(n, x) {
-  x.splice(n, 1);
-  console.log(x);
-}
